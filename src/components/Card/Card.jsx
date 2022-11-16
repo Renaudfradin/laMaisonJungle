41e32b3731c3d6db4af2481b76829761b2b39c80
Renaud <renaudfradin@gmail.com>
2022-11-16 22:22:53 +0100

    add useState

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,33 @@
 import react from 'react'
-function Card(props) {
+import { useState } from 'react'
+
+export default function Card() {
     const monsteraPrice = 8
     const ivyPrice = 10
     const flowerPrice = 15
+    const [card, updateCard ] = useState(0);
+    const [isOpen, IsClose ] = useState(false)
+
+
     return (
-        <div className="card">
-            <h2>Panier</h2>
+        
+        <div>
+            {isOpen ?  
+            <div className="card">
+                <p onClick={()=> IsClose(false)}>fermer</p>
+                <p onClick={()=> updateCard(0)}>vider</p>
+                <h2>Panier</h2>
                 <ul>
                     <li>Monstera : {monsteraPrice}€</li>
                     <li>Lierre : {ivyPrice}€</li>
                     <li>Fleurs : {flowerPrice}€</li>
                 </ul>Total : {monsteraPrice + ivyPrice + flowerPrice }€
+                <button onClick={()=> updateCard(card+1)}>ajouter </button>
+                <p>total {monsteraPrice * card} €</p>
+            </div>
+            
+            : <p onClick={()=> IsClose(true)}>ouvrir</p>}
         </div>
+        
     )
 }
-
-export default Card
